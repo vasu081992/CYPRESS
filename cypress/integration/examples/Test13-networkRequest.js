216935f266df2463665a94a8c32e3b1b4f0320cc
vasuve_altair <vasuve@altair.com>
2024-10-01 14:27:31 +0530

    cypress consolidated code

--- cypress/integration/examples/Test13-networkRequest.js
+++ cypress/integration/examples/Test13-networkRequest.js
@@ -9,24 +9,65 @@ it('Mocking Network Response Test1',()=>{
   cy.visit('https://rahulshettyacademy.com/angularAppdemo/')
 
 
- cy.intercept({
-  method:'GET',
-  url:'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty',
- },
-{
-  statusCode:200,
-  body:[{
-          "book_name": "RestAssured with Java",
-          "isbn": "BSG",
-          "aisle": "2302"
-       },{
-         "book_name": "Cypress with JS AND CUCUMBER",
-         "isbn": "BSG2",
-         "aisle": "23022"
-     }
-    ],
+//  cy.intercept({
+//   method:'GET',
+//   url:'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty',
+//  },
+// {
+//   statusCode:200,
+//   body:[{
+//           "book_name": "RestAssured with Java",
+//           "isbn": "BSG",
+//           "aisle": "2302"
+//        },{
+//          "book_name": "Cypress with JS AND CUCUMBER",
+//          "isbn": "BSG2",
+//          "aisle": "23022"
+//      }
+//     ],
 
+// }).as('bookRetrievals')
+
+// cy.intercept('GET', 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty', (req) => {
+//   req.continue((res) => {
+//     expect(res.statusCode).to.eq(200); // Optional: Ensure you have a successful response.
+ 
+//     console.log(res.body)
+
+//     expect(res.body).to.be.an('array');
+
+//     expect(res.body).to.deep.include.members([
+//       {
+//         book_name: 'RestAssured with Java',
+//         isbn: 'LSA',
+//         aisle: '2303'
+//       }
+//     ]) 
+
+//     expect(res.body[6].book_name).to.eq('Learn Appium Automation with Java');
+
+
+//     // Use a Cypress command to log the response body outside of the promise chain
+//     // After the request is resolved
+// })
+// }).as('bookRetrievals')
+
+cy.intercept('GET', 'https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty', (req) => {
+  // statusCode defaults to `302`
+  req.redirect('https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=malhotra', 301)
 }).as('bookRetrievals')
+
+// cy.intercept('GET','https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty', {
+// statusCode:200,
+// body:[{
+//   "book_name": "RestAssured with Java",
+//            "isbn": "BSG",
+//           "aisle": "2302"
+// }]
+// }
+// ).as('bookRetrievals')
+
+
 
 cy.get('button[class="btn btn-primary"]').click()
 
@@ -37,23 +78,26 @@ cy.get('button[class="btn btn-primary"]').click()
 //length of response array should match with table rows --> kind of integration testing 
 
 
-cy.wait('@bookRetrievals').then(({request,response})=>{
+ cy.wait('@bookRetrievals').then(({request,response})=>{
 
 
-  if(response.statusCode==200){
-  cy.get('tr').should('have.length',response.body.length+1)
-  }
-  else{
-    cy.get('tr').should('have.length',1)
+//   if(response.statusCode==200){
+//   cy.get('tr').should('have.length',response.body.length+1)
+//   }
+//   else{
+//     cy.get('tr').should('have.length',1)
 
-  }
-})
+//   }
+// })
 
 
 //cy.get('p').should('have.text','Oops only 1 Book available') // UNCOMMENT if we are stubbing only with one response
 
 
-cy.get('p').should('not.exist'); //uncomment this if we are gonna stub 2 responses ie we wont see error any more
+//cy.get('p').should('not.exist'); //uncomment this if we are gonna stub 2 responses ie we wont see error any more
+expect(response.statusCode).to.equal(301);
+
+})
 
 })
 
